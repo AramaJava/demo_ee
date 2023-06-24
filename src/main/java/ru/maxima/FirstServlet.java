e6edfc33f0a4cf82040a0e81798259a6b0597312
AramaJava <abr-aram@yandex.ru>
2023-06-24 14:23:29 +0500

    first servlet 1

--- src/main/java/ru/maxima/FirstServlet.java
+++ src/main/java/ru/maxima/FirstServlet.java
@@ -1,20 +1,29 @@
 package ru.maxima;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class FirstServlet extends HttpServlet {
     private String message;
+
+   /* ТЕКСТ ЗАДАНИЯ
+    Сделать JSP с презентацией себя,
+    где в заголовке будет ваша ФИО,
+    в <head></head> будет краткая информация о вас,
+    и в <body></body> будет 2-3 строки о вас чуть более полно.
+    Необходимо информацию в теле страницы сделать минимум 2 размерами/шрифтами
+    */
 
     public void init() {
         message = "It's from Servlet";
     }
+
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
 
@@ -24,7 +33,4 @@ public class FirstServlet extends HttpServlet {
         pw.println("</body></html>");
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
 }
